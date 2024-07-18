278af9d5e7573fe2f55892bc128b83bf07f999fa
Kayky <112106334+kaykypg7@users.noreply.github.com>
2024-07-18 17:52:15 -0300

    Adicionando pagina de Sobre

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -3,12 +3,14 @@ import { Routes } from '@angular/router';
 import { CalendarioComponent } from './calendario/calendario.component';
 import { InicioComponent } from './inicio/inicio.component';
 import { NewsComponent } from './news/news.component';
+import { SobreComponent } from './sobre/sobre.component';
 
 export const routes: Routes = [
 
     { path: '', redirectTo: 'inicio', pathMatch: 'full'}, 
     { path: 'inicio', component:InicioComponent},
     { path: 'news', component:NewsComponent},
-    { path: 'calendario', component:CalendarioComponent}
+    { path: 'calendario', component:CalendarioComponent},
+    { path: 'sobre', component:SobreComponent}
     
 ];
